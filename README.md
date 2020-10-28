d8f6b71812201d7783da5af822c6a434a6ced29d
bojue <lansemaikefeng@126.com>
2020-10-28 17:16:55 +0800

    update webpack.dev 环境搭建

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # M-Charts
-TODO：基于Canvas的Charts实现
+基于Canvas的Charts实现的技术预研
+
+## 下载
+
+```
+git clone https://github.com/bojue/M-Charts.git
+
+cd M-Charts
+
+npm i
+```
+
+## 运行
+
+```
+npm run dev
+
+```
---
+++ webpack.dev.conf.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/script/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+        "React": "react",
+    }),
+    new htmlWebpackPlugin({
+        template: path.join(__dirname, './public/index.html'),
+        filename:'index.html',
+    })
+  ],
+  watch:true,
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 3000,
+    hot:true,
+    host:"localhost",
+    historyApiFallback: true,
+    compress: true,
+    open:"Chrome",
+    openPage:''
+  },
+};
