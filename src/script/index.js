f814d4d8e17f12db87346a99dbfe6b603c53084f
bojue <lansemaikefeng@126.com>
2020-10-28 17:47:43 +0800

    初始化饼图

--- src/script/index.js
+++ src/script/index.js
@@ -2,21 +2,27 @@ let init = function() {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext('2d');
     ctx.beginPath();
-    ctx.fillStyle='green';
+    ctx.fillStyle='#4988FE';
     ctx.moveTo(300, 300);
-    ctx.arc(300, 300, 180, 0, Math.PI / 8);
+    ctx.arc(300, 300, 180, 0, Math.PI / 4);
     ctx.fill();
 
     ctx.beginPath();
     ctx.moveTo(300, 300);
-    ctx.fillStyle='red';
-    ctx.arc(300, 300, 180, Math.PI, 1.5 * Math.PI );
+    ctx.fillStyle='#22D3AA';
+    ctx.arc(300, 300, 180, Math.PI / 4,  Math.PI );
     ctx.fill();
 
     ctx.beginPath();
     ctx.moveTo(300, 300);
-    ctx.fillStyle='blue';
-    ctx.arc(300, 300, 180, 1.5 * Math.PI, 2 * Math.PI );
+    ctx.fillStyle='#00b33c';
+    ctx.arc(300, 300, 180, Math.PI ,  Math.PI * 1.4 );
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(300, 300);
+    ctx.fillStyle='#4944FE';
+    ctx.arc(300, 300, 180, 1.4 * Math.PI, 2 * Math.PI );
     ctx.fill();
 }
 
