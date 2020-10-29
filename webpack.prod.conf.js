5afb1e8d2e91711f057b8cc46bf16399e494a92f
bojue <lansemaikefeng@126.com>
2020-10-29 17:39:26 +0800

    update

--- webpack.prod.conf.js
+++ webpack.prod.conf.js
@@ -45,7 +45,7 @@ module.exports = {
         new htmlWebpackPlugin({
             template: "./public/index.html",
             filename:'index.html',
-            favicon:'./public/favicon.ico'
+            favicon:'./dist/favicon.ico'
         }),
         new CleanWebpackPlugin(
             {
