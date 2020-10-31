ded9a8a80331c4b7bd2000b08985c77cd8ba1597
bojue <lansemaikefeng@126.com>
2020-10-31 11:36:07 +0800

    update

--- src/pages/charts-list/charts-list.tsx
+++ src/pages/charts-list/charts-list.tsx
@@ -5,14 +5,24 @@ import "./charts-list.scss";
 let imgPie = require('./../../assets/imgs/shape/pie.png');
 
 class ChartsList extends React.Component<{}, object>  {
+    charts:any[] = [
+        {
+            name:"饼图",
+            type:'pie',
+            thumbnail: imgPie && imgPie.default,
+        },
+    ]
     render() {
+        const items = this.charts.map((item) => {
+           return  <a href="#/detail" key={item.key}>
+            <div className="charts-item">
+                <div className="name">饼图</div>
+                <img src={imgPie && imgPie.default} alt=""/>
+            </div>
+        </a>
+        })
         return  <div className="list">
-             <a href="#/detail">
-                <div className="charts-item">
-                            <div className="name">饼图</div>
-                            <img src={imgPie && imgPie.default} alt=""/>
-                        </div>
-                </a>
+                {items}
             </div>
         }
     }
