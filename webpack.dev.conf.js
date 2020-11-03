657c0702c32afec2525386f0d5998ea48e8f366d
bojue <lansemaikefeng@126.com>
2020-11-03 13:05:47 +0800

    update webpack

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -56,7 +56,7 @@ module.exports = {
     contentBase: path.join(__dirname, 'public'),
     port: 3000,
     hot:true,
-    host:"10.169.42.84",
+    host:"localhost",
     historyApiFallback: true,
     compress: false,
     open:"Chrome",
